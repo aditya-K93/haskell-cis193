b8da53ccf60ed27ccb7be3c1e9b6bc23dc7d11d7
aditya-K93 <aditya.kr.singh21@gmail.com>
2019-09-02 20:16:00 +0530

    added stream solutions

--- chapter6/Fibonacci.hs
+++ chapter6/Fibonacci.hs
@@ -10,3 +10,21 @@ fib1 = map fib [0 ..]
 
 fib2 :: [Integer]
 fib2 = fibFast 0 1 where fibFast x y = x : fibFast y (x + y)
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+    show = show . take 20 . streamToList
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
+
